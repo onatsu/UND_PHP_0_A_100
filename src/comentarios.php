35c5b439a3bd9df44b7d40e87f4fc5c5732a47ff
csi00687 <jmcruz@csi.uned.es>
2025-03-27 13:33:28 +0100

    Dia4-parte1

---
+++ src/comentarios.php
@@ -0,0 +1,7 @@
+Clase que lea el fichero CSV y que devuelva un array con los datos
+    - CSVDataReader
+    - CSVDataImporter
+
+Clase que filtre los datos y devuelva aquellos a los que hay que enviar la notificación
+Clase que envía la notificación
+
---
+++ src/index.php
@@ -0,0 +1,12 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+
+use App\CSVDataReader;
+
+$fileName="datos.csv";
+$dataReader=new CSVDataReader($fileName);
+
+$data=$dataReader->getData();
+print_r($data[0]);
+
+
