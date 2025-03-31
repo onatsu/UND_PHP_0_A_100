c2fac86e7102fc6672043179d80babf8a90182bc
josenovillo-uned <jm.novillo@csi.uned.es>
2025-03-31 14:06:10 +0200

    Care [id]: Refactor Rename Client FilterByData

--- src/SendBirthdayGreetingsToUsersUseCase.php
+++ src/SendBirthdayGreetingsToUsersUseCase.php
@@ -6,17 +6,17 @@ use App\DataFilterByBirthday;
 class SendBirthdayGreetingsToUsersUseCase
 {
     private $clientProvider;
-    private $filter;
+    private $filterByDate;
 
-    public function __construct(CSVDataReader $clientProvider, DataFilterByBirthday $filter){
+    public function __construct(CSVDataReader $clientProvider, DataFilterByBirthday $filterByDate){
         $this->clientProvider = $clientProvider;
-        $this->filter = $filter;
+        $this->filterByDate = $filterByDate;
     }
 
     public function execute()
     {
         $data=$this->clientProvider->getData();
 
-        print_r($this->filter->filter($data));
+        print_r($this->filterByDate->filter($data));
     }
 }
