0859f210edd638acc3a3584cf86c455669c4919e
David Cruz <info@davidcruz.net>
2025-04-02 12:35:53 +0200

    Feature[taskId]:adds constructors to value objects

--- src/rpg/Level.php
+++ src/rpg/Level.php
@@ -4,5 +4,12 @@ namespace App\rpg;
 
 class Level
 {
+    private int $level;
+
+    public function __construct(int $level)
+    {
+        $this->level = $level;
+    }
+
     public int $actualLevel = 1;
 }
--- src/rpg/Name.php
+++ src/rpg/Name.php
@@ -6,6 +6,11 @@ class Name
 {
     private string $name;
 
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
     public function getName(): string
     {
         return $this->name;
